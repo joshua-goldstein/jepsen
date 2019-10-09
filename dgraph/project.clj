a4c04b0b26250c81afed1605b6da8dd806cd470d
Martin Martinez Rivera <martinmr@dgraph.io>
2019-10-09 16:13:42 -0700

    Use local version of dgraph4j
    This version is using the last working versions of grpc and opencensus.

--- dgraph/project.clj
+++ dgraph/project.clj
@@ -3,20 +3,23 @@
   :url "https://github.io/jepsen-io/jepsen"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :plugins [[lein-localrepo "0.5.4"]]
+  :repositories [[local {:url "file:localrepo"
+                         :checksum :ignore}]]
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [jepsen "0.1.14-SNAPSHOT"]
                  [clj-http "3.7.0"]
                  [cheshire "5.8.0"]
                  ; Note that we specify manual versions of dgraph deps since
                  ; grpc uses version ranges
-                 [io.dgraph/dgraph4j "2.0.1"
+                 [io.dgraph/dgraph4j "2.0.2"
                   :exclusions [io.grpc/grpc-core
                                io.netty/netty-codec-http2]]
                  [io.grpc/grpc-core "1.22.1"]
-                 [io.netty/netty-codec-http2 "4.1.39.Final"]
+                 [io.netty/netty-codec-http2 "4.1.16.Final"]
                  [clj-wallhack "1.0.1"]
-                 [io.opencensus/opencensus-api  "0.23.0"]
-                 [io.opencensus/opencensus-impl "0.23.0"]
-                 [io.opencensus/opencensus-exporter-trace-jaeger "0.23.0"]]
+                 [io.opencensus/opencensus-api  "0.18.0"]
+                 [io.opencensus/opencensus-impl "0.18.0"]
+                 [io.opencensus/opencensus-exporter-trace-jaeger "0.18.0"]]
   :main jepsen.dgraph.core
   :profiles {:uberjar {:aot :all}})
